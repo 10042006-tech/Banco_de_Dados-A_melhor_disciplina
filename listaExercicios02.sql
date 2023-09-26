bea71d10f139506933ad119cb00257f56e692b0d
10042006-tech <98918134+10042006-tech@users.noreply.github.com>
2023-09-25 23:43:55 -0300

    listaExercicios02.sql

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -5,3 +5,13 @@ BEGIN
 END;
 // DELIMITER ;
 CALL sp_ListarAutores();
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN nome VARCHAR(150))
+BEGIN
+	SELECT Titulo FROM Livro
+	INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+	AND Categoria.Nome = nome;
+END;
+// DELIMITER ;
+CALL sp_LivrosPorCategoria('Autoajuda');
