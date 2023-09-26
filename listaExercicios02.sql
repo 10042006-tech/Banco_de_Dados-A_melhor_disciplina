9970ed55854db4fdde2c988da768d2b9625e00cd
10042006-tech <98918134+10042006-tech@users.noreply.github.com>
2023-09-25 23:45:18 -0300

    listaExercicios02.sql

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -15,3 +15,15 @@ BEGIN
 END;
 // DELIMITER ;
 CALL sp_LivrosPorCategoria('Autoajuda');
+
+DELIMITER //
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN nome VARCHAR(150))
+BEGIN
+	SELECT Categoria.Nome, COUNT(*) AS quantidade_livros
+	FROM Livro 
+	INNER JOIN Categoria ON Categoria.Categoria_ID = Livro.Categoria_ID 
+	WHERE Categoria.Nome = nome
+	GROUP BY Nome;
+END;
+// DELIMITER ;
+CALL sp_ContarLivrosPorCategoria('Romance');
